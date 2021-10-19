bd170405549828b42f8588987012f430e7f03466
Filipe Brandão <fdabrandao@gmail.com>
2021-10-19 12:52:48 +0100

    Build on manylinux using as many threads as available

--- manylinux/build.sh
+++ manylinux/build.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 if [ "$#" -eq 0 ]; then
-    echo "Usage: $0 [32|64]"
+    echo "Usage: $0 nproc arch [32|64]"
 elif [ -d "/base/manylinux/" ]; then
-    BUILD_DIR=/base/manylinux/linux$1/
+    NRPOC=$1
+    ARCH=$2
+    NBITS=$3
+    BUILD_DIR=/base/manylinux/linux-$ARCH/
     rm -rf $BUILD_DIR
     mkdir -p $BUILD_DIR
     cd $BUILD_DIR
     cp -r /base /tmp/coin
-    cmake /tmp/coin/ -DARCH=$1
-    make all coin-versions -j2
+    cmake /tmp/coin/ -DARCH=$NBITS # -DNO_AVX_HARDWARE=1
+    make all coin-versions -j$NPROC
     make test || true
+    make package
 else
     echo "Must be run inside a docker container."
 fi
--- manylinux/run.sh
+++ manylinux/run.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 cd `dirname $0`
 
-rm -rf linux32/*
-rm -rf linux64/*
-mkdir linux32 linux64
-docker run -v `pwd`/../:/base --rm fdabrandao/manylinux32:coin_cmake-3.12.4 /base/manylinux/build.sh 32
-docker run -v `pwd`/../:/base --rm fdabrandao/manylinux64:coin_cmake-3.12.4 /base/manylinux/build.sh 64
+NPROC=`nproc`
+
+rm -rf linux-/*
+
+docker run -v `pwd`/../:/base --rm fdabrandao/manylinux32:coin_cmake-3.12.4 /base/manylinux/build.sh $NPROC intel32 32
+docker run -v `pwd`/../:/base --rm fdabrandao/manylinux64:coin_cmake-3.12.4 /base/manylinux/build.sh $NPROC intel64 64
+
+# docker run -v `pwd`/../:/base --rm dockcross/linux-armv7 /base/manylinux/build.sh $NPROC armv7 32
+# docker run -v `pwd`/../:/base --rm fdabrandao/manylinux2014_aarch64:cmake_3.12.4 /base/manylinux/build.sh $NPROC arm64 arm64
+# docker run -v `pwd`/../:/base --rm dockcross/manylinux2014-aarch64 /base/manylinux/build.sh $NPROC arm64 arm64
+# docker run -v `pwd`/../:/base --rm messense/manylinux2014-cross:armv7l /base/manylinux/build.sh $NPROC arm64 arm64
+# docker run -v `pwd`/../:/base --rm fdabrandao/manylinux2014_aarch64:cmake_3.12.4  /base/manylinux/build.sh $NPROC arm64 arm64
+
+
